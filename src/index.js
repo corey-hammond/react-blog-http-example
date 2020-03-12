8ceef75f82acad376893201c7b197df28c297466
Corey Hammond <coreyjhammond@gmail.com>
2020-03-11 18:28:45 -0700

    setup app for routing

--- src/index.js
+++ src/index.js
@@ -1,35 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import axios from "axios";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
-// set up global config
-// axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com/'
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
 axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
-axios.defaults.headers.post['Content-Type'] = 'application/JSON'
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-// set up interceptors 
-axios.interceptors.request.use(
-  request => {
+axios.interceptors.request.use(request => {
     console.log(request);
-    // Can edit request config
+    // Edit request config
     return request;
-  },
-  error => {
+}, error => {
     console.log(error);
     return Promise.reject(error);
-  }
-);
+});
 
 axios.interceptors.response.use(response => {
     console.log(response);
+    // Edit request config
     return response;
 }, error => {
     console.log(error);
-    return Promise.reject(error)
-})
+    return Promise.reject(error);
+});
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
